71eb08a37424265e104992b804930039391d6eeb
Vitor <valencastrol@gmail.com>
2018-03-06 18:00:00 -0300

    Ataque

---
+++ Assets/Resources/Scripts/SpriteChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteChanger : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+
+	// Use this for initialization
+	void Start () {
+		ChangeSprite ();
+	}
+
+	void OnEnable(){
+		ChangeSprite ();
+	}
+
+	void ChangeSprite(){
+		SpriteRenderer sr = this.GetComponent<SpriteRenderer> ();
+		sr.sprite = sprites [Random.Range (0, sprites.Length)];
+	}
+}
